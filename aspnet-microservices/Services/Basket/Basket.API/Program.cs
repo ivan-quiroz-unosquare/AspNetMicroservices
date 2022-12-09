fa7123cd4169a2fea22da9f3a1512361a06e7933
ivan-quiroz-unosquare <118236094+ivan-quiroz-unosquare@users.noreply.github.com>
2022-12-08 21:49:45 -0600

    Consume Discount gRPC Serive from Basket Service

--- aspnet-microservices/Services/Basket/Basket.API/Program.cs
+++ aspnet-microservices/Services/Basket/Basket.API/Program.cs
@@ -1,4 +1,6 @@
+using Basket.API.GrpcServices;
 using Basket.API.Repositories;
+using Discount.gRPC.Protos;
 
 namespace Basket.API
 {
@@ -14,6 +16,11 @@ namespace Basket.API
                 options.Configuration = builder.Configuration.GetValue<string>("CacheSettings:ConnectionString");
             });
             builder.Services.AddScoped<IBasketRepository, BasketRespository>();
+            builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(options =>
+            {
+                options.Address = new Uri(builder.Configuration.GetValue<string>("GrpcSettings:DiscountUrl"));
+            });
+            builder.Services.AddScoped<DiscountGrpcService>();
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
